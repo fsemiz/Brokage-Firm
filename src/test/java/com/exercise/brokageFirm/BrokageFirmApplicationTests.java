454dc4a272bf698f75b0e1ef57e7f928e2a0ff89
fatih.semiz <fatihsemiz@aselsan.com.tr>
2025-03-22 09:28:06 +0300

    noIssue(BrokageFirm):initialize the project

---
+++ src/test/java/com/exercise/brokageFirm/BrokageFirmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exercise.brokageFirm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrokageFirmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
